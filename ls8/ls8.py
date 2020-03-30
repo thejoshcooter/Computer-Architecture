4337a38c6410aa35c46231210094dcff7018ada1
Josh Cooter <thejoshcooter@gmail.com>
2020-03-30 01:29:21 -0500

    complete

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,11 @@
 import sys
 from cpu import *
 
+if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} filename", file=sys.stderr)
+        sys.exit(1)
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
